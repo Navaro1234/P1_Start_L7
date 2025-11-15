519d0b221c9f18774d634ca77930c37f6d21fa50
Navaro Devos <164686546+Navaro1234@users.noreply.github.com>
2025-11-15 15:30:38 +0100

    gemaakt

---
+++ startcode/b_steden.py
@@ -0,0 +1,11 @@
+steden = ("Roeselare", "Beveren", "Kortrijk", "Tokio", "Amsterdam")
+
+def print_steden(steden):
+    print("Mijn gekozen steden zijn:")
+    for steden in steden:
+        print(steden)
+
+
+
+
+print_steden(steden)
